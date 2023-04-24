0455906a5596601c18e7e7e42d8a5ad8d72a1e30
omartins-zs <gabrielmatheus209@gmail.com>
2023-04-24 15:20:11 -0300

    Criando logica para Adicionar Classe Dark Mode

---
+++ Dark_Mode_Simples/js/script.js
@@ -0,0 +1,7 @@
+
+const body = document.querySelector('body')
+const checkbox = document.querySelector('#switch')
+
+checkbox.addEventListener('change', function () {
+    body.classList.toggle('dark-mode')
+})
--- Toggle_Button_js copy/js/script.js
+++
@@ -1,7 +0,0 @@
-
-const html = document.querySelector('html')
-const checkbox = document.getElementById('switch')
-
-checkbox.addEventListener('change', function () {
-    html.classList.toggle('dark-mode')
-})
