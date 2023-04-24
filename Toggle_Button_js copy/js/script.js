1fa8e837982356aa1c1466d8a8a0bafb6ed8536a
omartins-zs <gabrielmatheus209@gmail.com>
2023-04-24 15:03:22 -0300

    Criando Logica para Ativar dark mode

---
+++ Toggle_Button_js copy/js/script.js
@@ -0,0 +1,7 @@
+
+const html = document.querySelector('html')
+const checkbox = document.getElementById('switch')
+
+checkbox.addEventListener('change', function () {
+    html.classList.toggle('dark-mode')
+})
